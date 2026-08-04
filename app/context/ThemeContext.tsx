ad77db93e5c255fed4e63bf7af5fc417bd0817fb
ipixfun <onlinezoezoe@gmail.com>
2026-08-05 01:12:43 +0700

    plo

--- app/context/ThemeContext.tsx
+++ app/context/ThemeContext.tsx
@@ -7,10 +7,10 @@ export const AVAILABLE_THEMES = [
   "emerald-cream",
   "teal-coral",
   "sea-citrus",
-  "raisin-sunset",
+  "ivory-lime",
   "gunmetal-platinum",
   "charcoal-ecru",
-  "charcoal-sage",
+  "lime-charcoal",
   "cyber-neon",
   "custom",
 ] as const;
